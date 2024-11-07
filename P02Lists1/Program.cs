15e8672f48e256732b5673f4e7dc8313b9749712
izzetcosar <izzetcosar@gmail.com>
2024-11-08 02:03:18 +0300

    .

--- P02Lists1/Program.cs
+++ P02Lists1/Program.cs
@@ -1,4 +1,4 @@
-﻿List<string>ziyaretciler = new List<string>()
+﻿List<string>ziyaretciler = new List<string>() //Ziyaretçileri eklediğimiz listemiz
 {
     "Bülent Ersoy",
     "Ajda Pekkan",
@@ -11,11 +11,11 @@
 };
 
 Console.WriteLine("**Davetliler**");
-for (int i = 0; i < ziyaretciler.Count; i++)
+for (int i = 0; i < ziyaretciler.Count; i++) //listeleri for döngüsü ile ekrana yazdırıyoruz
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.Write($"{i+ 1}.");
-    Console.ResetColor();
-    Console.WriteLine( ziyaretciler[i]);
+    Console.ForegroundColor = ConsoleColor.Red; //yazımıza font color ekliyoruz.
+    Console.Write($"{i+ 1}."); //sayilari 1 arttırarak listemizi sıralıyoruz.
+    Console.ResetColor(); //isimlerde default renk kullanılmasını istediğimiz için renk kodunu resetliyoruz.
+    Console.WriteLine( ziyaretciler[i]); // Listemizde olusturduğumuz elemanları döndürüyoruz.
 }
 Console.ReadKey();
