84b8b8ad08754fb394648d726b7950aab3e6c65c
izzetcosar <izzetcosar@gmail.com>
2024-11-08 01:35:54 +0300

    güncelleme

--- P01Array/Program.cs
+++ P01Array/Program.cs
@@ -3,30 +3,38 @@ Console.WriteLine("Diziye 10 adet sayi giriniz: ");
 
 for (int i = 0; i < Sayilar.Length; i++)        //For döngüsüyle 0 dan başlayarak elemanları kullanıcıya yazdırıyoruz.
 {
-    Console.Write($"Sayı[{i}] =");          
+    Console.Write($"Sayı[{i}] =");
     Sayilar[i] = int.Parse(Console.ReadLine());
 
 }
-Console.Clear();        //yazdırdıkdan sonra ekranı temizledik
+Console.Clear();       //yazdırdıkdan sonra ekranı temizledik
+
 foreach (var a in Sayilar)      //foreach ile dizi elemanlarını ekrana yazdırdık.
 {
     Console.WriteLine(a);
 
 }
+
+Console.Clear();
 Console.WriteLine("Dizimize 11.Elemanı Ekleyiniz:"); //11. elemanımızı ekliyoruz
-int yeniDeger = int.Parse(Console.ReadLine());
+int yeniSayi = int.Parse(Console.ReadLine());
 
-List<int> sayiListesi = Sayilar.ToList(); 
-sayiListesi.Add(yeniDeger);
-Sayilar = sayiListesi.ToArray();
+int[] yeniSayilar = new int[Sayilar.Length + 1];
 
-Array.Sort(Sayilar);        //sort ile sayılarımızı sıraladık
-Array.Reverse(Sayilar);     //reserve komutuyla sıralamamızı tersine çevirdik.
 
+for (int i = 0; i < Sayilar.Length; i++)
+{
+    yeniSayilar[i] = Sayilar[i];
+}
+yeniSayilar[yeniSayilar.Length - 1] = yeniSayi;
+
+Array.Sort(yeniSayilar);
+Array.Reverse(yeniSayilar);
 
 Console.WriteLine("\nDizinin Büyükten Küçüğe Sıralı Hali:");
-foreach (var eleman in Sayilar)
+foreach (var b in yeniSayilar)
 {
-    Console.WriteLine(eleman);
+    Console.WriteLine(b);
 }
+
 Console.ReadKey();
