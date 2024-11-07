78d0df110a9010f00d421fed078b5509f02313de
izzetcosar <izzetcosar@gmail.com>
2024-11-08 01:54:07 +0300

    P02Lists1 eklendi

---
+++ P02Lists1/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string>ziyaretciler = new List<string>()
+{
+    "Bülent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gündeş",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın"
+};
+
+Console.WriteLine("**Davetliler**");
+for (int i = 0; i < ziyaretciler.Count; i++)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($"{i+ 1}.");
+    Console.ResetColor();
+    Console.WriteLine( ziyaretciler[i]);
+}
+Console.ReadKey();
---
+++ P02Lists1/README.md
@@ -0,0 +1,4 @@
+# Pratik - Lists 1
+**Patika Plus gala gecesine davetlisiniz!**
+
+- Bu Pratik'te sizden ziyaretçilerin isimlerini eklediðimiz bir liste oluþturmanýzý ve bu liste içerisinde for ya da foreach döngüsü ile dönerek isimlerini tek tek ekrana yazdýrmanýz isteniyor.
