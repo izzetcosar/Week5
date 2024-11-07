abb68feb6211fa87c97a72c79099ba8123f5a87f
izzetcosar <izzetcosar@gmail.com>
2024-11-08 02:21:40 +0300

    P03Lists2 eklendi

---
+++ P03Lists2/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string>kahveler = new List<string>();   //kahveler adında bir liste oluşturduk
+Console.WriteLine("Lütfen Sevdiğiniz Kahveleri Giriniz: ");
+
+for (int i = 0; i < 5; i++)  //For döngüsü ile kullanıcıdan sırasıyla sevdiği kahveleri istiyoruz
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($"Kahve {i + 1}:"); //liste sırasını 1 arttırarak yazdırıyoruz.
+    Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Green;
+    string kahve = Console.ReadLine(); //kullanıcıdan kahvesini yazdırıyoruz
+    kahveler.Add( kahve );
+    Console.ResetColor();
+}
+Console.WriteLine("Favori Kahveleriniz:"); 
+foreach(var kahve in kahveler)      //foreach ile olusturduğunuz listemizi yazdırıyoruz
+{
+    Console.WriteLine(kahve);
+}
---
+++ P03Lists2/README.md
@@ -0,0 +1,3 @@
+# Pratik - Lists 2
+### Bir kahve içsek bize iyi gelecek!
+-kullanýcýnýn konsoldan gireceði 5 kahve ismiyle bir liste oluþturup, bu listeyi foreach döngüsüyle yazdýralým.
