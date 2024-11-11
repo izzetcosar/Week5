8ae2fda179b344479734008e96475c22a7d97f9d
izzetcosar <izzetcosar@gmail.com>
2024-11-11 15:41:23 +0300

    P05TryCatch eklendi

---
+++ P05TryCatch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int kare = sayi * sayi;
+            Console.WriteLine("Girdiğiniz sayının karesi: " + kare);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş!");
+        }
+        Console.ReadKey();
+    }
+    
+}
---
+++ P05TryCatch/README.md
@@ -0,0 +1,6 @@
+# Pratik - Try - Catch
+**Bu pratikte basit bir try-catch uygulamasý yapalým.
+
+ Kullanýcýdan bir sayý girmesini isteyen bir program yazýn. Kullanýcý geçerli bir sayý girdiðinde bu sayýnýn karesini ekrana yazdýrýnýz. 
+
+Kullanýcý geçersiz bir giriþ yaparsa (sayý yerine harf veya sembol girmesi gibi), "Geçersiz giriþ! Lütfen bir sayý giriniz." þeklinde bir hata mesajý gösteriniz.**
